d9c7e154beca2fd446d219b72f588ae3fb7bb54e
yonetomo <yonetomo.asics@gmail.com>
2019-06-05 19:10:35 +0900

    postgresをインストールしてsrttingsに対応させた

--- django_app/settings.py
+++ django_app/settings.py
@@ -74,12 +74,35 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+#管理ユーザーid:admin　pw:admin
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         # 'default': {
+         # 'ENGINE': 'django.db.backends.sqlite3',
+         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         # }
+           'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'lim',
+           'USER': 'postgres',
+           'PASSWORD': 'admin',
+           'HOST': '127.0.0.1',
+           'POST': '5432'
+         }
 }
+
+
+
+
+
 
 
 # Password validation
